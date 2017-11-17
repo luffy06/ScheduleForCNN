932a933b0cb7dae0631eb1671a78732021ae234c
wushangyu <534427411@qq.com>
2017-11-18 04:30:11 +0800

    修改脚步

--- code/test.sh
+++ code/test.sh
@@ -6,10 +6,13 @@ fi
 for file in ${datafolder}/*${suffix}; do
   filename=`basename $file`
   echo 'Dealing with '$filename >> result.out
-  for (( i = 1; i <= 3; i++ )); do
-    echo '######### Using Theory'$i' #########' >> result.out
-    echo `./run$i < $datafolder/$filename` >> result.out
-    echo '######### End #########' >> result.out
-  done
+  # for (( i = 2; i <= 3; i++ )); do
+  #   echo '######### Using Theory'$i' #########' >> result.out
+  #   echo `./run$i < $datafolder/$filename` >> result.out
+  #   echo '######### End #########' >> result.out
+  # done
+  echo '######### Using TheoryDAC #########' >> result.out
+  echo `./runDAC < $datafolder/$filename` >> result.out
+  echo '######### End #########' >> result.out
   echo '' >> result.out
 done
