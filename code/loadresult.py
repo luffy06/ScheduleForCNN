dd8c6f6079f02c8fffed8ba9463112262399470e
wushangyu <534427411@qq.com>
2020-05-19 20:44:45 +0800

    添加实验结果

--- code/loadresult.py
+++ code/loadresult.py
@@ -6,7 +6,7 @@ resultexcel = 'output.xlsx'
 algorithms = ['lctes', 'resc', 'bala']
 pes = [16, 32, 64]
 attributes = ['Benchmark', 'Node', 'Edge']
-metrics = ['TotalTime', 'Prologue', 'TimePerRound', 'RunOnCacheN', 'CPURatio', 'Cost']
+metrics = ['TotalTime', 'Prologue', 'TimePerRound', 'CPURatio', 'RunOnCacheN', 'RunOnCache', 'Cost']
 drop_benchmark = []
 
 def parse_graph(line):
